d74bd6cb2d51aebfb2a38f7a64edc024515bd2c4
agnatio <120420991+agnatio@users.noreply.github.com>
2024-08-04 17:59:30 +0300

    Update directory_structure.py
    second direct commit

--- directory_structure.py
+++ directory_structure.py
@@ -3,6 +3,7 @@ import os
 
 def list_files(
     startpath,
+    output_file,
     remove_objects=[
         "_delete/",
         "__pycache__",
@@ -12,29 +13,30 @@ def list_files(
     ],
     max_levels=None,
 ):
-    print("./")  # Initial display for the root directory
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write("./\n")  # Initial display for the root directory
 
-    for root, dirs, files in os.walk(startpath):
-        for obj in remove_objects:
-            if obj in dirs:
-                dirs.remove(obj)
+        for root, dirs, files in os.walk(startpath):
+            for obj in remove_objects:
+                if obj in dirs:
+                    dirs.remove(obj)
 
-        level = root.replace(startpath, "").count(os.sep)
+            level = root.replace(startpath, "").count(os.sep)
 
-        # If max_levels is set, skip levels beyond it
-        if max_levels is not None and level > max_levels:
-            continue
+            # If max_levels is set, skip levels beyond it
+            if max_levels is not None and level > max_levels:
+                continue
 
-        # Adjust the indentations using the new characters
-        indent = " " * 4 * (level - 1) + "├── " if level > 0 else ""
-        subindent = " " * 4 * level + "│   ├── "
+            # Adjust the indentations using the new characters
+            indent = " " * 4 * (level - 1) + "├── " if level > 0 else ""
+            subindent = " " * 4 * level + "│   ├── "
 
-        # Skip printing the root directory again
-        if root != startpath:
-            print("{}{}/".format(indent, os.path.basename(root)))
+            # Skip printing the root directory again
+            if root != startpath:
+                f.write("{}{}/\n".format(indent, os.path.basename(root)))
 
-        for f in files:
-            print("{}{}".format(subindent, f))
+            for file in files:
+                f.write("{}{}\n".format(subindent, file))
 
 
 if __name__ == "__main__":
@@ -46,5 +48,6 @@ if __name__ == "__main__":
         "alembic",
         ".pytest_cache",
     ]
+    output_file = "folders.txt"
 
-    list_files(".", max_levels=3, remove_objects=remove_objects)
+    list_files(".", output_file, max_levels=3, remove_objects=remove_objects)
